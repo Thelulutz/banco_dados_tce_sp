3dde19af0561d731338bebbb9dee125532a76b99
Thelulutz <lf.bassetto95@gmail.com>
2020-10-03 10:11:11 -0300

    Ajuste do format e de tabs

--- projeto/projeto/pipelines.py
+++ projeto/projeto/pipelines.py
@@ -26,19 +26,19 @@ class TempoPipeline(object):
     def process_item(self, item, spider):
         try:
             self.cur.execute("""insert into dim_tempo(dt_data, ano, nr_semestre, nr_trimestre, nr_mes, nm_mes, nr_dia, nm_dia, evento_especial) 
-                                values(%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                                values({},{},{},{},{},{},{},{},{})
                                 on conflict (dt_data)
-                                do nothing;""",
-                             (item['Data'],
-                              item['Ano'],
-                              item['Nr_Semestre'],
-                              item['Nr_Trimestre'],
-                              item['Nr_Mes'],
-                              item['Nm_Mes'],
-                              item['Nr_dia'],
-                              item['Nm_Dia'],
-                              item['Ev_Especial']
-                              ))
+                                do nothing;""".format(
+                                item['Data'],
+                                item['Ano'],
+                                item['Nr_Semestre'],
+                                item['Nr_Trimestre'],
+                                item['Nr_Mes'],
+                                item['Nm_Mes'],
+                                item['Nr_Dia'],
+                                item['Nm_Dia'],
+                                item['Ev_Especial']
+                            ))
             self.connection.commit()
             return item
         except:
@@ -61,10 +61,10 @@ class GastoPipeline(object):
 
     def process_item(self, item, spider):
         try:
-            self.cur.execute("""insert into dim_gasto(nr_empenho, vl_gasto) values(%s,%s);""",
-                             (item['Nr_Empenho'],
-                              item['Vl_Gasto']
-                              ))
+            self.cur.execute("""insert into dim_gasto(nr_empenho, vl_gasto) values({},{});""".format(
+                                item['Nr_Empenho'],
+                                item['Vl_Gasto']
+                            ))
             self.connection.commit()
             return item
         except:
@@ -89,13 +89,13 @@ class FornecedorPipeline(object):
         try:
             self.connection.commit()
             self.cur.execute("""insert into dim_fornecedor(cod_fornecedor, nm_fornecedor, nr_cnpj) 
-                                values(%s,%s,%s)
+                                values({},{},{})
                                 on conflict (nm_fornecedor)
-                                do nothing ;""",
-                             (item['Cd_Fornecedor'],
-                              item['Nm_Fornecedor'],
-                              item['Nr_CNPJ']
-                              ))
+                                do nothing ;""".format(
+                                item['Cd_Fornecedor'],
+                                item['Nm_Fornecedor'],
+                                item['Nr_CNPJ']
+                            ))
             return item
         except:
             pass
@@ -118,11 +118,9 @@ class OrgaoPipeline(object):
     def process_item(self, item, spider):
         try:
             self.cur.execute("""insert into dim_orgao_publico(nm_orgao) 
-                                values(%s) 
+                                values({}) 
                                 on conflict (nm_orgao)
-                                do nothing ;""",
-                             (item['Nm_Orgao']
-                              ))
+                                do nothing ;""".format(item['Nm_Orgao']))
             self.connection.commit()
             return item
         except:
@@ -146,12 +144,12 @@ class LocalPipeline(object):
     def process_item(self, item, spider):
         try:
             self.cur.execute("""insert into dim_local(nm_municipio, sg_estado) 
-                                values(%s,%s) 
+                                values({},{}) 
                                 on conflict (nm_municipio) 
-                                do nothing ;""",
-                             (item['Nm_Municipio'],
-                              item['Sg_Estado']
-                              ))
+                                do nothing ;""".format(
+                                item['Nm_Municipio'],
+                                item['Sg_Estado']
+                            ))
             self.connection.commit()
             return item
         except:
@@ -174,10 +172,10 @@ class ReceitaPipeline(object):
 
     def process_item(self, item, spider):
         try:
-            self.cur.execute("""insert into dim_receita(ft_receita, vl_receita) values(%s,%s);""",
-                             (item['Ft_Receita'],
-                              item['Vl_Receita']
-                              ))
+            self.cur.execute("""insert into dim_receita(ft_receita, vl_receita) values({},{});""".format(
+                                item['Ft_Receita'],
+                                item['Vl_Receita']
+                            ))
             self.connection.commit()
             return item
         except:
